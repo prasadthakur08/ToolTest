8ebbb17ca69b87e9ba3dd8cd36c3f38b06e890b2
Thakur Prasad <thakur.prasad08@gmail.com>
2016-08-07 08:49:31 +0530

    modified Printng message.......

--- GitHubDemo/src/prasad/demo/tool/Test.java
+++ GitHubDemo/src/prasad/demo/tool/Test.java
@@ -1,7 +1,16 @@
 package prasad.demo.tool;
 
 public class Test {
-public static void main(String[] args){
-	System.out.println("Git hub demo");
-}
+	
+	public String testMethod(){
+		return "my name is thakur Prasad";
+	}
+	
+	public static void main(String[] args){
+		System.out.println("Git hub demo");
+		System.out.println("Hello java");
+		//test method execution
+		Test test=new Test();
+		System.out.println(test.testMethod());
+	}
 }
